9b59e8952d9fa3e3f75363a32918183e90a5b01a
ejaramillom <ejaramilo@hotmail.com>
2020-02-21 11:42:04 -0500

    Added mongoDB functionality

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const app = express();
+const mongoose = require("mongoose");
+// mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mydatabase', { useNewUrlParser: true });
+mongoose.connection.on("error", function(e) { console.error(e); });
+// el resto del código
+// definimos el schema
+const schema = mongoose.Schema({
+  date: { type: Date, default: Date.now },
+  name: String,
+});
+// definimos el modelo
+const Visitor = mongoose.model("Visitor", schema);
 
-app.get('/', (req, res) => {
-  res.send('<h1>HcUy6Re2LLBRtj</h1>');
+app.get('/:nombre', (req, res) => {
+  let nombre = (!req.params.nombre || req.params.nombre.length === 0 ? "Anónimo" : req.params.nombre);
+  Visitor.create({ name: nombre }, function(err) {
+    if (err) return console.error(err);
+  });
+  res.send ('<h1>El visitante ${nombre} fue almacenado con éxito</h1>');
 });
 
-app.listen(3000, () => console.log('Listening on port 3000!'))
+Visitor.find(function(err, response){
+   console.log(response);
+});
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
